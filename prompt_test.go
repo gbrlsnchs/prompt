99724918e4fee7d5de32c08fbffc7ee3fe156eb8
Gabriel Sanches <g@lonemaker.com>
2019-06-08 12:06:25 -0300

    prompt: add confirmation with statuses tests

---
+++ prompt_test.go
@@ -0,0 +1,140 @@
+package prompt_test
+
+import (
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/gbrlsnchs/prompt"
+)
+
+func TestPromptConfirmStatus(t *testing.T) {
+	testCases := []struct {
+		r       io.Reader
+		accept  []string
+		decline []string
+
+		msg  string
+		want prompt.Status
+	}{
+		// default "y"
+		{
+			r:       strings.NewReader("y\n"),
+			accept:  []string{"y", "yes"},
+			decline: []string{"n", "no"},
+			msg:     "Confirm? [y(es)/n(o)] ",
+			want:    prompt.StatusAccept,
+		},
+		// default "n"
+		{
+			r:       strings.NewReader("n\n"),
+			accept:  []string{"y", "yes"},
+			decline: []string{"n", "no"},
+			msg:     "Are you sure? [y(es)/n(o)] ",
+			want:    prompt.StatusDecline,
+		},
+		// capital "y"
+		{
+			r:       strings.NewReader("Y\n"),
+			accept:  []string{"y", "yes"},
+			decline: []string{"n", "no"},
+			msg:     "Is that right? [y(es)/n(o)] ",
+			want:    prompt.StatusAccept,
+		},
+		// capital "n"
+		{
+			r:       strings.NewReader("N\n"),
+			accept:  []string{"y", "yes"},
+			decline: []string{"n", "no"},
+			msg:     "Accept all? [y(es)/n(o)] ",
+			want:    prompt.StatusDecline,
+		},
+		// default "yes"
+		{
+			r:       strings.NewReader("yes\n"),
+			accept:  []string{"y", "yes"},
+			decline: []string{"n", "no"},
+			msg:     "Confirm? [y(es)/n(o)] ",
+			want:    prompt.StatusAccept,
+		},
+		// default "no"
+		{
+			r:       strings.NewReader("no\n"),
+			accept:  []string{"y", "yes"},
+			decline: []string{"n", "no"},
+			msg:     "Are you sure? [y(es)/n(o)] ",
+			want:    prompt.StatusDecline,
+		},
+		// capital "yes"
+		{
+			r:       strings.NewReader("YES\n"),
+			accept:  []string{"y", "yes"},
+			decline: []string{"n", "no"},
+			msg:     "Is that right? [y(es)/n(o)] ",
+			want:    prompt.StatusAccept,
+		},
+		// capital "no"
+		{
+			r:       strings.NewReader("NO\n"),
+			accept:  []string{"y", "yes"},
+			decline: []string{"n", "no"},
+			msg:     "Accept all? [y(es)/n(o)] ",
+			want:    prompt.StatusDecline,
+		},
+		// default whitespaced "yes"
+		{
+			r:       strings.NewReader("  yes  \n"),
+			accept:  []string{"y", "yes"},
+			decline: []string{"n", "no"},
+			msg:     "Confirm? [y(es)/n(o)] ",
+			want:    prompt.StatusAccept,
+		},
+		// default whitespaced "no"
+		{
+			r:       strings.NewReader("  no  \n"),
+			accept:  []string{"y", "yes"},
+			decline: []string{"n", "no"},
+			msg:     "Are you sure? [y(es)/n(o)] ",
+			want:    prompt.StatusDecline,
+		},
+		// capital whitespaced "yes"
+		{
+			r:       strings.NewReader("  YES  \n"),
+			accept:  []string{"y", "yes"},
+			decline: []string{"n", "no"},
+			msg:     "Is that right? [y(es)/n(o)] ",
+			want:    prompt.StatusAccept,
+		},
+		// capital whitespaced "no"
+		{
+			r:       strings.NewReader("  NO  \n"),
+			accept:  []string{"y", "yes"},
+			decline: []string{"n", "no"},
+			msg:     "Accept all? [y(es)/n(o)] ",
+			want:    prompt.StatusDecline,
+		},
+		// none
+		{
+			r:       strings.NewReader("foo\n"),
+			accept:  []string{"y", "yes"},
+			decline: []string{"n", "no"},
+			msg:     "Please answer yes or no: ",
+			want:    prompt.StatusNone,
+		},
+	}
+	var stdin strings.Builder
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			stdin.Reset()
+			p := prompt.New(tc.r, &stdin)
+			p.SetAccept(tc.accept...)
+			p.SetDecline(tc.decline...)
+			if want, got := tc.want, p.ConfirmStatus(tc.msg); want != got {
+				t.Errorf("want %d, got %d", want, got)
+			}
+			if want, got := tc.msg, stdin.String(); want != got {
+				t.Errorf("want %q, got %q", want, got)
+			}
+		})
+	}
+}
