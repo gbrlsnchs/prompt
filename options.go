8e544013d854842ea2030d0b5affa3e9e813cf8e
Gabriel Sanches <gabriel@gsr.dev>
2019-07-13 18:20:14 -0300

    options: resolve case insensitive transformation

--- options.go
+++ options.go
@@ -3,14 +3,23 @@ package prompt // import "gsr.dev/prompt"
 import (
 	"bufio"
 	"io"
+	"strings"
 )
 
 // Option is a functional option that enables
 // setting private properties to a Prompt pointer.
 type Option func(*Prompt)
 
-// Stdin sets a new input buffer.
-func Stdin(r io.Reader) Option {
+// CaseInsensitive transforms answers to lowercase.
+func CaseInsensitive(p *Prompt) {
+	prev := p.transform
+	p.transform = func(s string) string {
+		return strings.ToLower(prev(s))
+	}
+}
+
+// ReadFrom sets a new input buffer.
+func ReadFrom(r io.Reader) Option {
 	return func(p *Prompt) {
 		p.sc = bufio.NewScanner(r)
 	}
---
+++ options_test.go
@@ -0,0 +1,98 @@
+package prompt_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"gsr.dev/prompt"
+)
+
+func TestOptions(t *testing.T) {
+	t.Run("CaseInsensitive", func(t *testing.T) {
+		testCases := []struct {
+			name string
+			in   prompt.Inputs
+			want prompt.Answer
+		}{
+			{
+				name: "y",
+				in:   testIn,
+				want: prompt.True,
+			},
+			{
+				name: "n",
+				in:   testIn,
+				want: prompt.False,
+			},
+			{
+				name: "Y",
+				in:   testIn,
+				want: prompt.Undefined,
+			},
+			{
+				name: "N",
+				in:   testIn,
+				want: prompt.Undefined,
+			},
+			{
+				name: "yes",
+				in:   testIn,
+				want: prompt.True,
+			},
+			{
+				name: "no",
+				in:   testIn,
+				want: prompt.False,
+			},
+			{
+				name: "YES",
+				in:   testIn,
+				want: prompt.Undefined,
+			},
+			{
+				name: "NO",
+				in:   testIn,
+				want: prompt.Undefined,
+			},
+			{
+				name: "__yes__",
+				in:   testIn,
+				want: prompt.True,
+			},
+			{
+				name: "__no__",
+				in:   testIn,
+				want: prompt.False,
+			},
+			{
+				name: "__YES__",
+				in:   testIn,
+				want: prompt.Undefined,
+			},
+			{
+				name: "__NO__",
+				in:   testIn,
+				want: prompt.Undefined,
+			},
+			{
+				name: "foo",
+				in:   testIn,
+				want: prompt.Undefined,
+			},
+		}
+		for _, tc := range testCases {
+			t.Run("", func(t *testing.T) {
+				f, err := os.Open(fmt.Sprintf("testdata/inputs/%s.txt", tc.name))
+				if err != nil {
+					t.Fatal(err)
+				}
+				os.Stdin = f
+				p := prompt.New()
+				if want, got := tc.want, p.Answer(tc.in); got != want {
+					t.Errorf("want %d, got %d", want, got)
+				}
+			})
+		}
+	})
+}
